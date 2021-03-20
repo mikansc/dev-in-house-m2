668e7f4d8fd270d80fbfba471e1d7c9fc200d3fe
mikansc <michael.nsc@outlook.com>
2021-03-20 18:19:04 -0300

    [SP13] Exercício 14

---
+++ src/exercicio14/Retangulo.java
@@ -0,0 +1,49 @@
+package exercicio14;
+
+public class Retangulo {
+    private double comprimento;
+    private double largura;
+
+    public Retangulo() {
+        comprimento = 1;
+        largura = 1;
+    }
+
+    public double getComprimento() {
+        return comprimento;
+    }
+
+    // retorna boolean porque ainda não vimos disparo de exceções
+    public boolean setComprimento(double comprimento) {
+        if(comprimento > 0.00 && comprimento < 20.0) {
+            this.comprimento = comprimento;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public double getLargura() {
+        return largura;
+    }
+
+    // retorna boolean porque ainda não vimos disparo de exceções
+    public boolean setLargura(double largura) {
+        if(largura > 0.00  && largura < 20.0) {
+            this.largura = largura;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public double getArea() {
+        return this.comprimento * this.largura;
+    }
+
+    public double getPerimetro(){
+        return (this.comprimento + this.largura) * 2;
+    }
+
+
+}
