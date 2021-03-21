192c74c085ec68f87ab48aaa6fb918a0d572670b
mikansc <michael.nsc@outlook.com>
2021-03-21 18:44:26 -0300

    [SP14] Exercício 8

---
+++ src/semana2/exercicio8/Funcionario.java
@@ -0,0 +1,16 @@
+package semana2.exercicio8;
+
+public class Funcionario {
+    private String nome;
+    private String sobrenome;
+    private String cpf;
+    private double salario;
+
+    public Funcionario(String nome, String sobrenome, String cpf, double salario) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.cpf = cpf;
+        this.salario = salario;
+    }
+
+}
