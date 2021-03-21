0fdced6c28801b05546d48dd3076c759aa54d7da
mikansc <michael.nsc@outlook.com>
2021-03-20 22:50:08 -0300

    [SP13] Exercício 17

---
+++ src/exercicio17/ContaPoupanca.java
@@ -0,0 +1,24 @@
+package exercicio17;
+
+public class ContaPoupanca {
+    private static double taxaDeJurosAnual;
+
+    private double saldo;
+
+    public ContaPoupanca(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void calcularJuroMensal() {
+        this.saldo += (saldo * taxaDeJurosAnual) / 12;
+    }
+
+    public static void alterarTaxaAnual(double novaTaxa) {
+        taxaDeJurosAnual = novaTaxa;
+    }
+
+}
