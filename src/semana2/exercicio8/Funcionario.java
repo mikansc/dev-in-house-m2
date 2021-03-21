ea8dac15c63594133b556bb2214beab5da9c89ac
mikansc <michael.nsc@outlook.com>
2021-03-21 18:51:00 -0300

    [SP14] Exercício 9

--- src/semana2/exercicio8/Funcionario.java
+++ src/semana2/exercicio8/Funcionario.java
@@ -9,8 +9,45 @@ public class Funcionario {
     public Funcionario(String nome, String sobrenome, String cpf, double salario) {
         this.nome = nome;
         this.sobrenome = sobrenome;
-        this.cpf = cpf;
-        this.salario = salario;
+        this.cpf = cpf.length() == 11 ? cpf : "00000000000";
+        this.salario = salario > 0 ? salario : 0.00;
     }
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+    // validação de CPF simples (número de dígitos);
+    public void setCpf(String cpf) {
+        if(cpf.length() == 11) {
+            this.cpf = cpf;
+        } else {
+            this.cpf = "00000000000";
+        }
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        if(salario > 0) {
+            this.salario = salario;
+        }
+    }
 }
